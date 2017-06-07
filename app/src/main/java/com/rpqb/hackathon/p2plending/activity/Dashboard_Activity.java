f4004a7d0e6ac81ea50f2d7b9e7303709954e707
Vikramv <Vikramv@corp.rpqb.com>
2017-06-07 20:52:54 +0530

    Splash screen integrated

---
+++ app/src/main/java/com/rpqb/hackathon/p2plending/activity/Dashboard_Activity.java
@@ -0,0 +1,18 @@
+package com.rpqb.hackathon.p2plending.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.rpqb.hackathon.p2plending.R;
+
+/**
+ * Created by Vikramv on 6/7/2017.
+ */
+
+public class Dashboard_Activity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_activity);
+    }
+}
---
+++ app/src/main/java/com/rpqb/hackathon/p2plending/activity/SplashScreen_Activity.java
@@ -0,0 +1,74 @@
+package com.rpqb.hackathon.p2plending.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.rpqb.hackathon.p2plending.R;
+
+/**
+ * Created by Vikramv on 6/7/2017.
+ */
+
+public class SplashScreen_Activity extends Activity {
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+
+    /**
+     * Called when the activity is first created.
+     */
+    Thread splashTread;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen_activity);
+        StartAnimations();
+    }
+
+    private void StartAnimations() {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        animation.reset();
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.splashscreen_linearLayout);
+        linearLayout.clearAnimation();
+        linearLayout.startAnimation(animation);
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.translate);
+        animation.reset();
+        ImageView iv = (ImageView) findViewById(R.id.splashscreen_imgViewSplash);
+        iv.clearAnimation();
+        iv.startAnimation(animation);
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashScreen_Activity.this,
+                            Dashboard_Activity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashScreen_Activity.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    SplashScreen_Activity.this.finish();
+                }
+            }
+        };
+        splashTread.start();
+    }
+}
